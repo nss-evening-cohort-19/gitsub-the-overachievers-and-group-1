fc35f87b0795c17a5a98e17ba611f3ba3159c614
Krima Patel <patel.krima@hotmail.com>
2022-05-10 18:18:18 -0500

    added data to packages.js

---
+++ .github/packages.js
@@ -0,0 +1,31 @@
+const packagesData = [
+  {
+    id: 1,
+    packName: "Docker",
+    packDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  }, 
+  {
+    id: 2,
+    packName: "RubyGems",
+    packDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  }, 
+  {
+    id: 3,
+    packName: "Apache Maven",
+    packDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+    id: 4, 
+    packName: "npm",
+    packDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  }, {
+    id: 5,
+    packName: "NuGet",
+    packDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  }, 
+  {
+    id: 6,
+    packName: "Containers",
+    packDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  }
+];
