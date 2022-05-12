b0dd3e8399529b9991a527d423aeebe578409c02
Phantom-Farmer <joeljdahl@gmail.com>
2022-05-11 21:08:54 -0500

    all basic elements rendered to my page

---
+++ scripts-main/data.js
@@ -0,0 +1,84 @@
+
+const users = [
+  {
+    id: 0,
+    name: 'Chris P. Bacon',
+    userName: 'chrissyPBacc89',
+    userPhoto: "./images/chrispbacon.png",
+    userBio: "Likes to party",
+    userLocation: 'Paris, France',
+    userEmail: 'chrispbacon@yahoo.com',
+  },
+  {
+    id: 1,
+    name: 'Lawrence B. Gillybee',
+    userName: 'biglawtheman@netscape.com',
+    userPhoto: "./images/lawrencebgillybee.png",
+    userBio: "Likes to party too",
+    userLocation: 'Paris, Texas',
+    userEmail: 'lawrencebgillybee@yahoo.com',
+  },
+  {
+    id: 2,
+    name: 'Madeline Smith',
+    userName: 'maddysmith55',
+    userPhoto: "./images/madelinesmith.png",
+    userBio: "Likes to party as well",
+    userLocation: 'Paris, Chicago',
+    userEmail: 'madelinesmith@yahoo.com',
+  },
+]
+
+export {users}
+
+/*const projectObjects = [
+ 
+  {
+  pk: 0,
+  userId: 0,
+  projName: "Pet Adoption",
+  projDesc: "Design an app to aid in finding homes for pets",
+  update: "33 minutes ago",
+  status: "Old McDonald has a farm",
+ },
+
+ {
+  pk: 1,
+  userId: 1,
+  projName: "Video Player",
+  projDesc: "Build a working YouTube style video player",
+  update: "420 hours ago",
+  status: "net scripts and chill",
+ },
+
+ {
+  pk: 2,
+  userId: 2,
+  projName: "Sorting Hat",
+  projDesc: "Design an app that will randomly select houses for new students at Hogworts School of Magic",
+  update: "666 days ago",
+  status: "Hokus Pokus - I've been expelled",
+ },
+
+ {
+  pk: 3,
+  userId: 3,
+  projName: "Product Cards",
+  projDesc: "Build an app to aid in finding homes for used instruments",
+  update: "9 years ago",
+  status: "beginner lessons initiated",
+
+ },
+
+ {
+  pk: 4,
+  userId: 4,
+  projName: "First Group Project",
+  projDesc: "Keep doing what you are doing, because you're currently doing your part in the first group project",
+  update: "1 second ago",
+  status: "All in this shit together",
+ },
+
+];
+
+export {projectObjects}*/
